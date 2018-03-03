9ab3b4ec2fcbf754ffc64f1657a2cedf4bbf71be
Tobias Bleckert <hola@tobiasbleckert.se>
2018-03-03 23:53:05 +0100

    Started with testing

---
+++ __tests__/popup.test.js
@@ -0,0 +1,25 @@
+import test from 'ava';
+import React from 'react';
+import Popup from '../src/Popup.react';
+import {shallow} from 'enzyme';
+
+test('Initialization works', t => {
+    const component = shallow(<Popup className="popup" />);
+
+    t.true(component.hasClass('popup'));
+    t.is(component.find('.popup__overlay').length, 1);
+});
+
+test('Display and hide popup', t => {
+    const component = shallow(<Popup className="popup" />);
+
+    Popup.create({});
+    component.update();
+
+    t.is(component.find('.popup__box').length, 1);
+
+    Popup.close();
+    component.update();
+
+    t.is(component.find('.popup__box').length, 0);
+});
