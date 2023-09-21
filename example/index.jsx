9af9d60471c1fc75a88defed118d2ab5a989a937
Tobias Bleckert <hola@tobiasbleckert.se>
2023-09-21 13:32:16 +0200

    Support react 18

--- example/index.jsx
+++ example/index.jsx
@@ -1,36 +1,41 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React, {useEffect} from 'react';
+import { createRoot } from 'react-dom/client';
 import Popup from '../src';
 import '../style.css';
 
-render(
-    <Popup />,
-    document.querySelector('#root'),
-);
+const root = createRoot(document.querySelector('#root'));
 
-Popup.create({
-    title: 'Hello World!',
-    onShow: (id, title) => {
-        console.log('Callback: onShow', id, title);
-    },
-    onClose: (id, title) => {
-        console.log('Callback: onClose', id, title);
-    },
-    content: (
-        <div>
-            It takes more than just a good looking body. You've got to have the heart and soul to go with it.
-        </div>
-    ),
-    className: 'alert',
-    buttons: {
-        left: ['cancel'],
-        right: [
-            <span style={{padding: '0 15px'}}>🦄</span>,
-            {
-                text: 'Ok!',
-                className: 'success',
-                action: Popup.close
-            }
-        ]
-    },
-});
+function App() {
+    useEffect(() => {
+        Popup.create({
+            title: 'Hello World!',
+            onShow: (id, title) => {
+                console.log('Callback: onShow', id, title);
+            },
+            onClose: (id, title) => {
+                console.log('Callback: onClose', id, title);
+            },
+            content: (
+                <div>
+                    It takes more than just a good looking body. You've got to have the heart and soul to go with it.
+                </div>
+            ),
+            className: 'alert',
+            buttons: {
+                left: ['cancel'],
+                right: [
+                    <span style={{padding: '0 15px'}}>🦄</span>,
+                    {
+                        text: 'Ok!',
+                        className: 'success',
+                        action: Popup.close
+                    }
+                ]
+            },
+        });
+    }, []);
+
+    return <Popup />;
+}
+
+root.render(<App />);
